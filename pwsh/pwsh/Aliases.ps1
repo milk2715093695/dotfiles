c778401388a6f049023ca430dfc66ad195a401fb
milk2715093695 <miao_renke@qq.com>
2026-04-16 01:49:20 +0800

    refactor(pwsh): separate aliases and functions

--- pwsh/pwsh/Aliases.ps1
+++ pwsh/pwsh/Aliases.ps1
@@ -1,25 +1,6 @@
 # 兼容 Unix 风格的命令
 Set-Alias python3 python -Scope Global
 
-# 兼容 Unix 风格的 ll 命令
-function ll {
-    Get-ChildItem -Force | Format-Table -AutoSize
-}
-
-# 自动懒加载的 conda 函数
-function conda {
-    . "$HOME\.config\pwsh\Conda.ps1"
-    Remove-Item Function:conda -ErrorAction SilentlyContinue
-    & conda @Args
-}
-
-# yazi 的包装器
-function y {
-	$tmp = (New-TemporaryFile).FullName
-	yazi.exe $args --cwd-file="$tmp"
-	$cwd = Get-Content -Path $tmp -Encoding UTF8
-	if ($cwd -ne $PWD.Path -and (Test-Path -LiteralPath $cwd -PathType Container)) {
-		Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
-	}
-	Remove-Item -Path $tmp
-}
+# 用 nvim 作为默认编辑器
+Set-Alias vim nvim -Scope Global
+Set-Alias vi nvim -Scope Global
--- pwsh/pwsh/Functions.ps1
+++ pwsh/pwsh/Functions.ps1
@@ -102,3 +102,26 @@ function Import-Secrets {
         Write-Host "无效输入" -ForegroundColor Red
     }
 }
+
+# 兼容 Unix 风格的 ll 命令
+function ll {
+    Get-ChildItem -Force | Format-Table -AutoSize
+}
+
+# 自动懒加载的 conda 函数
+function conda {
+    . "$HOME\.config\pwsh\Conda.ps1"
+    Remove-Item Function:conda -ErrorAction SilentlyContinue
+    & conda @Args
+}
+
+# yazi 的包装器
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi.exe $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if ($cwd -ne $PWD.Path -and (Test-Path -LiteralPath $cwd -PathType Container)) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
