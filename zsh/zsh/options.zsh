d9cd593926757529f496a59019670a816ce76615
milk2715093695 <miao_renke@qq.com>
2026-08-13 19:51:08 +0800

    feat(zsh): enable extended history with save-no-dups

--- zsh/zsh/options.zsh
+++ zsh/zsh/options.zsh
@@ -12,7 +12,9 @@ SAVEHIST=10000
 # 行为优化
 setopt INC_APPEND_HISTORY      # 命令执行后立即写入
 setopt SHARE_HISTORY           # 多终端共享
-setopt HIST_IGNORE_ALL_DUPS    # 完全去重
+setopt EXTENDED_HISTORY        # 历史记录时间戳
+setopt HIST_IGNORE_ALL_DUPS    # 完全去重（内存）
+setopt HIST_SAVE_NO_DUPS       # 去重（写盘时不再写入重复项）
 setopt HIST_REDUCE_BLANKS      # 去掉多余空格
 
 # 允许用 $EDITOR 编辑当前命令行
